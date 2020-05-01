6fa8395b18a43e5e1f5e75d1dcf32d5c3eb59fd5
EC2 Default User <ec2-user@ip-172-31-17-175.us-east-2.compute.internal>
2020-05-01 07:20:06 +0000

    Putting categories in place

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipename'
+app.config["MONGO_URI"] = 'mongodb+srv://benbourgeois:Thibodaux985@recipes-wjt0j.mongodb.net/recipename?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World...again'
+@app.route('/recipe_list')
+def recipe_list():
+    return render_template("recipes.html", Content=mongo.db.Content.find())
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
