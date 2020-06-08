89d77c39af641d273c2677cbf91df0ea7973ccda
benbourgeois <bourgeoisben@ymail.com>
2020-06-08 19:00:00 +0000

    Fixing Edit and Delete Buttons, Added Edit Recipe

--- app.py
+++ app.py
@@ -26,18 +26,34 @@ def insert_recipe():
     Content.insert_one(request.form.to_dict())
     return redirect(url_for('recipe_list'))
 
-@app.route('/favorites')
-def favorites():
-    return render_template('favorites.html',
-    Content=mongo.db.Content.find())
+@app.route('/edit_recipe/<content_id>')
+def edit_recipe(content_id):
+    the_content = mongo.db.Content.find_one({"_id": ObjectId(content_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editrecipe.html', content=the_content, categories=all_categories)
 
-@app.route('/add_favorites', methods=['POST'])
-def add_favorites():
+@app.route('/update_recipe/<content_id>', methods=["POST"])
+def update_recipe(content_id):
     Content=mongo.db.Content
-    Content.insert_one(request.form.to_dict())
-    return redirect(url_for('favorites.html'))
+    Content.update(
+        {'_id': ObjectId(content_id)},
+        {
+            'recipe_category': request.form.get('recipe_category'),
+            'recipe_name': request.form.get('recipe_name'),
+            'ingredients': request.form.get('ingredients'),
+            'steps': request.form.get('steps'),
+            'difficulty': request.form.get('difficulty'),
+            'first_name': request.form.get('first_name'),
+            'last_name': request.form.get('last_name'),
+        })
+    return redirect(url_for('recipe_list'))
+
+@app.route('/delete_recipe/<content_id>')
+def delete_recipe(content_id):
+    mongo.db.Content.remove({'_id': ObjectId(content_id)})
+    return redirect(url_for('recipe_list'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(5000),
+    port=int(3000),
     debug=True)
